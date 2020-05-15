383549f9706657e804c25f82d89481a864940e59
Tominous <18189490+Tominous@users.noreply.github.com>
2020-05-15 13:41:53 +0100

    Update EventManager.java

--- src/main/java/xiroc/dungeoncrawl/util/EventManager.java
+++ src/main/java/xiroc/dungeoncrawl/util/EventManager.java
@@ -22,16 +22,16 @@ public class EventManager {
 	
 	
 
-//	public static boolean createAssumption(String name) {
-//		for (String part : JsonConfig.ASSUMPTION_SEARCHLIST) {
-//			if (name.contains(part)) {
-//				DungeonCrawl.LOGGER.info(
-//						"The biome {} contains the sequence \"{}\". It will be blacklisted, assuming that it belogs to a different dimension than the overworld. If you are a modpack creator or thinking that this is wrong, consider configurating the biome-blacklists inside the DungeonCrawl/config.json file in the confg directory and clear the ASSUMPTION_SEARCHLIST.",
-//						name, part);
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
+	public static boolean createAssumption(String name) {
+		for (String part : JsonConfig.ASSUMPTION_SEARCHLIST) {
+			if (name.contains(part)) {
+				DungeonCrawl.LOGGER.info(
+						"The biome {} contains the sequence \"{}\". It will be blacklisted, assuming that it belogs to a different dimension than the overworld. If you are a modpack creator or thinking that this is wrong, consider configurating the biome-blacklists inside the DungeonCrawl/config.json file in the confg directory and clear the ASSUMPTION_SEARCHLIST.",
+						name, part);
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
